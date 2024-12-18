d310ece1a934ce2159f3a27a64a6aa8d1b6d704d
gahen97 <gahen97@gmail.com>
2024-12-18 01:20:03 -0500

    Added validation for task Schema, and logic for controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,60 @@
 const Task = require('../models/Task.js')
-const createTask = (req, res) => {
-    res.json(req.body)
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('Modify Task')
+const updateTask = async (req, res) => {
+    try {
+        const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).json({ msg: `ID : ${req.params.id} does not correspond to any Tasks` })
+        }
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('Delete Task')
+const deleteTask = async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id })
+        if (!task) {
+            return res.status(404).json({ msg: `ID : ${req.params.id} does not correspond to any Tasks` })
+        }
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
-const getTask = (req, res) => {
-    res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+    try {
+        const task = await Task.findOne({ _id: req.params.id })
+        if (!task) {
+            return res.status(404).json({ msg: `ID : ${req.params.id} does not correspond to any Tasks` })
+        }
+        res.status(200).json({ task })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
-const getAllTasks = (req, res) => {
-    res.send('All Items')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (err) {
+        res.status(500).json({ msg: err })
+    }
 }
 
 
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        maxlength: [40, 'Name is too long (Cannot be longer than 40 characters)'],
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
